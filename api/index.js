e8edde04fe0a23019b238ff7e575601174eef7d0
Daniel <williamsoligodaniel@gmail.com>
2023-11-09 13:23:27 +0100

    connect to MongoDB database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 app.listen(3000, () => {
   console.log('server is running on port 3000')
 })
+
+const mySecret = process.env.MONGO
+
+const client = mongoose.connect(mySecret);
+
+const connectToDatabase = async () => {
+    try {
+        await client
+        console.log(`Connected to MongoDB`)
+
+    } catch (err) {
+        console.error(`Error connecting to the database: ${err}`)
+    }
+}
+connectToDatabase();
