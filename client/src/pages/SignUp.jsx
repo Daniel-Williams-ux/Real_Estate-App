fa0af89a1ad587af6b3527a8f4d34b40405f82fb
Daniel <williamsoligodaniel@gmail.com>
2024-01-04 19:19:42 +0100

    update Complete sign up page functionality by adding setLoading, setError, and useNavigate

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -13,17 +16,30 @@ export default function SignUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    }); 
-    const data = await res.json();
-    console.log(data);
-  }
-console.log(formData);
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      }); 
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+    
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -32,7 +48,9 @@ console.log(formData);
         <input type='text' placeholder='username' name='username' autoComplete="username" className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
         <input type='email' placeholder='email' name='email' autoComplete="email" required aria-describedby="email-validation" className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
         <input type='password' placeholder='password' name='password' autoComplete="password" className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+         {loading ?  'Loading...' : 'Sign up' }
+        </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
@@ -40,6 +58,7 @@ console.log(formData);
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
